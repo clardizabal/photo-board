422544b3825ec9c8a48861e36ec322a5e5f450d5
clardizabal <chris.lardizabal@gmail.com>
2016-04-04 12:19:44 -0700

    add angular module to client

---
+++ client/app/app.js
@@ -0,0 +1,43 @@
+angular.module('photoboard', [
+  'ngRoute'
+])
+.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+    // .when('/signin', {
+    //   templateUrl: 'app/auth/signin.html',
+    //   controller: 'AuthController'
+    // })
+    // .when('/signup', {
+    //   templateUrl: 'app/auth/signup.html',
+    //   controller: 'AuthController'
+    // })
+    .when('/board',  {
+      templateUrl: 'app/board/board.html',
+      controller: 'BoardController'
+    });
+    // .when('/addphotos', {
+    //   templateUrl: 'app/addphotos/addphotos.html'
+    // });
+
+  $httpProvider.interceptors.push('AttachTokens');
+});
+// .factory('AttachTokens', function($window) {
+//   var attach = {
+//     request: function(object) {
+//       var jwt = $window.localStorage.getItem('com.photoboard');
+//       if (jwt) {
+//         object.headers['x-access-token'] = jwt;  
+//       }
+//       object.headers['Allow-Control-Allow-Origin'] = '*';
+//       return object;
+//     }
+//   };
+//   return attach;
+// })
+// .run(function($rootScope, $location, Auth) {
+//   $rootScope.$on('$routeChangeStart', function(evt, next, current) {
+//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+//       $location.path('/signin');
+//     }
+//   });
+// });
