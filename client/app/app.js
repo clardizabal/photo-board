e5456c2dc7b61c0bf4534af309ec0c1e12b4bfb5
clardizabal <chris.lardizabal@gmail.com>
2016-04-04 16:29:22 -0700

    add auth factory and controller

--- client/app/app.js
+++ client/app/app.js
@@ -1,18 +1,19 @@
 angular.module('photoboard', [
   'photoboard.services',
+  'photoboard.auth',
   'photoboard.board',
   'ngRoute'
 ])
 .config(function($routeProvider, $httpProvider) {
   $routeProvider
-    // .when('/signin', {
-    //   templateUrl: 'app/auth/signin.html',
-    //   controller: 'AuthController'
-    // })
-    // .when('/signup', {
-    //   templateUrl: 'app/auth/signup.html',
-    //   controller: 'AuthController'
-    // })
+    .when('/signin', {
+      // templateUrl: 'app/auth/signin.html',
+      // controller: 'AuthController'
+    })
+    .when('/signup', {
+      // templateUrl: 'app/auth/signup.html',
+      // controller: 'AuthController'
+    })
     .when('/board',  {
       templateUrl: 'app/board/board.html'
       // controller: 'BoardController'
@@ -21,25 +22,25 @@ angular.module('photoboard', [
       templateUrl: 'app/upload/upload.html'
     });
 
-  // $httpProvider.interceptors.push('AttachTokens');
+  $httpProvider.interceptors.push('AttachTokens');
+})
+.factory('AttachTokens', function($window) {
+  var attach = {
+    request: function(object) {
+      var jwt = $window.localStorage.getItem('com.photoboard');
+      if (jwt) {
+        object.headers['x-access-token'] = jwt;  
+      }
+      object.headers['Allow-Control-Allow-Origin'] = '*';
+      return object;
+    }
+  };
+  return attach;
+})
+.run(function($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function(evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
 });
-// .factory('AttachTokens', function($window) {
-//   var attach = {
-//     request: function(object) {
-//       var jwt = $window.localStorage.getItem('com.photoboard');
-//       if (jwt) {
-//         object.headers['x-access-token'] = jwt;  
-//       }
-//       object.headers['Allow-Control-Allow-Origin'] = '*';
-//       return object;
-//     }
-//   };
-//   return attach;
-// })
-// .run(function($rootScope, $location, Auth) {
-//   $rootScope.$on('$routeChangeStart', function(evt, next, current) {
-//     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
-//       $location.path('/signin');
-//     }
-//   });
-// });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -12,4 +12,43 @@ angular.module('photoboard.services', [])
   };
 
   return {getImages: getImages};
+})
+.factory('Auth', function($http, $location, $window) {
+  var signin = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signin',
+      data: user
+    })
+    .then(function(resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function(user) {
+    return $http({
+      method: 'POST',
+      url: '/api/users/signup',
+      data: user
+    })
+    .then(function(resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function() {
+    return !!$window.localStorage.getItem('com.photoboard');
+  };
+
+  var signout = function() {
+    $window.localStorage.removeItem('com.photoboard');
+    $location.path('/signin');
+  };
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout, signout
+  };
 });
