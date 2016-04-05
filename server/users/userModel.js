d431b5f97f1060d8ce5db9fbdd8e87ee8024d91f
clardizabal <chris.lardizabal@gmail.com>
2016-04-04 17:19:35 -0700

    create user mongodb model

---
+++ server/users/userModel.js
@@ -0,0 +1,61 @@
+var Q = require('q');
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var SALT = 10;
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+  salt: String
+});
+
+UserSchema.methods.comparePasswords = function (tryPassword) {
+  var savedPassword = this.password;
+  return Q.Promise(function(resolve, reject) {
+    bcrypt.compare(tryPassword, savedPassword, function(err, isMatch) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(isMatch);
+      }
+    });
+  });
+};c
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // hash new or modified passwords
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // generate a salt
+  bcrypt.genSalt(SALT, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+
+    // hash password + salt
+    bcrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      user.salt = salt;
+      next();
+    
+    });
+  });
+});
+
+module.exports = mongoose.model('users', UserSchema);
